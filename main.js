a89206df6d09f286c001c356f1cb84c0580f7246
DominicMorquecho <dominic.morquecho@gmail.com>
2014-09-05 13:51:44 -0700

    My portfolio with very small changes because i need more pushes

--- main.js
+++ main.js
@@ -2,4 +2,14 @@ document.querySelector( "#nav-toggle" )
   .addEventListener( "click", function() {
     this.classList.toggle( "active" );
   });
-  
+  $(document).ready(function() {
+ 
+  var owl = $("#owl-demo");
+ 
+  owl.owlCarousel({
+    navigation : true,
+    singleItem : true,
+    transitionStyle : "fade"
+  });
+ 
+});
